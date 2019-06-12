2d7081f8fe25548451a7edb95c456e7d6f5af6e2
Taylor McKenzie <taymcken@uat.edu>
2019-06-12 03:51:03 -0700

    Nothing New, Still Broken
    help

--- Project 2/Decimal to Hex Converter/Program.cs
+++ Project 2/Decimal to Hex Converter/Program.cs
@@ -23,6 +23,7 @@ namespace Decimal_to_Hex_Converter
             {
                 string userInput;
                 int userNum;
+                /*
                 do
                 {
                     Console.Write("Number to convert: ");
@@ -33,9 +34,19 @@ namespace Decimal_to_Hex_Converter
                         Console.WriteLine("Press any key to continue...");
                         Console.ReadKey(true);
                     }
+
                     else if (userInput != "q")
                     {
+                        for (int i = 0; i < userInput.Length; i++)
+                        {
+                            //if (userInput[i] != )
+                            {
+
+                            }
+                        }
+                        
                         userNum = Convert.ToInt32(userInput);
+                        //if (userInput )
                         Console.WriteLine("The user entered a number");
                         Console.WriteLine("Press any key to continue...");
                         Console.ReadKey(true);
@@ -44,10 +55,87 @@ namespace Decimal_to_Hex_Converter
                     {
                         Console.WriteLine("Please enter a numeric value, or 'q' to quit.");
                     }
-                } while (userInput != null);
-                
-                
-                
+                } //while (userInput == "q" || userNum >= 0 && userNum <= 1000000);
+
+                do
+                {
+                    Console.Write("Number to convert: ");
+                    userInput = Console.ReadLine();
+                    //userNum = Convert.ToInt32(userInput);
+
+                    if ()
+                    {
+                        Console.WriteLine("The user is quitting");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey(true);
+                    }
+
+                    else if (userInput != "q" || userInput != "Q" || userInput != "Quit" || userInput != "quit")
+                    {
+                        if (Convert.ToInt32(userInput) < 0 || Convert.ToInt32(userInput) > 1000000)
+                        {
+                            
+                        }
+                        
+                    }
+                    else
+                    {
+                        userNum = Convert.ToInt32(userInput);
+                        //if (userNum >=
+                    }
+                } while (userInput != null);*/
+
+                do
+                {
+                    //userInput = Console.ReadLine();
+                    //userNum = Convert.ToInt32(userInput);
+
+                    userNum = Convert.ToInt32(Console.ReadLine());
+                    
+
+
+                    if (userNum >= 0 && userNum <= 1000000)
+                    {
+                        Console.WriteLine("Getting a hex change");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey(true);
+                        //hexChange();
+                    }
+
+                    else
+                    {
+                        userInput = Convert.ToString(userNum);
+
+                        if (userInput == "q" || userInput == "Q" || userInput == "Quit" || userInput == "quit")
+                        {
+                            Console.WriteLine("User is quitting");
+                            Console.WriteLine("Press any key to continue...");
+                            Console.ReadKey(true);
+                        }
+
+                        else
+                        {
+                            System.Console.WriteLine("Please enter a valid number, or use 'q' to exit.");
+                        }
+                    }
+                    /*
+                    else if (userInput == "q" || userInput == "Q" || userInput == "Quit" || userInput == "quit")
+                    {
+                        Console.WriteLine("User is quitting");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey(true);
+                    }
+
+                    else
+                    {
+                        System.Console.WriteLine("Please enter a valid number, or use 'q' to exit.");
+                    }*/
+
+                } while (userNum != null);
+            }
+
+            void hexChange()
+            {
 
             }
         }
