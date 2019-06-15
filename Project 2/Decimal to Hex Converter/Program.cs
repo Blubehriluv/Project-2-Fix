00451944ddf5d1740af7b5396ef5bcec3ff76256
Taylor McKenzie <taymcken@uat.edu>
2019-06-15 02:13:08 -0700

    Finished Everything

--- Project 2/Decimal to Hex Converter/Program.cs
+++ Project 2/Decimal to Hex Converter/Program.cs
@@ -13,29 +13,34 @@ namespace Decimal_to_Hex_Converter
     {
         static void Main(string[] args)
         {
+            //Introductions
             Console.WriteLine("Welcome to the Decimal to Hex Converter!");
             Console.WriteLine("Enter a number to be converted.");
-            Console.WriteLine("Use the digits '000' to exit the program.");
-            
+            Console.WriteLine("Use 'q' to exit the program.");
+            int userNum;
+
             beginConverting();
 
-
+            //This begins the conversions
             void beginConverting()
             {
+                //The user's input is stored to be checked as a string
                 string userInput = Console.ReadLine();
 
+                //If the user is quitting, the program will exit.
                 if (userInput == "q")
                 {
                     Console.WriteLine("The user is quitting");
                     Console.WriteLine("Press enter to continue.");
                     Console.ReadLine();
                 }
+                //If the user isn't quitting and entering a number, the hexchange method will be called
                 else if (userInput != "q")
                 {
-                    int userNum = Convert.ToInt32(userInput);
+                    userNum = Convert.ToInt32(userInput);
+                    //The number must be between 0 and 1000000
                     if (userNum > 0 && userNum < 1000000)
                     {
-                        beginConverting();
                         hexChange();
                     }
                 }
@@ -43,7 +48,12 @@ namespace Decimal_to_Hex_Converter
 
             void hexChange()
             {
+                int placeHold = userNum;
 
+                string hexStuff = placeHold.ToString("X");
+                Console.WriteLine("You have converted {0} to {1}.", userNum, hexStuff);
+                Console.WriteLine("Please enter another number.");
+                beginConverting();
             }
         }
     }
